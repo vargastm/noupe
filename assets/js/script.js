97c58a22bcaf2e3c02bb8deea3e5ab3b2acc2625
Lucas Armindo <lucasarmindo@outlook.com>
2020-06-22 23:49:32 -0300

    Restore script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,38 +11,38 @@ function handleButtonFilter(){
   }
 }
 
-// const slide = new Slide('.slide', '.slide-wrapper');
-// slide.init(); 
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init(); 
 
-// class Gallery {
-//   constructor() {
-//     this.gallery = document.querySelector('[data-gallery="gallery"]');
-//     this.galleryList = document.querySelectorAll('[data-gallery="list"]');
-//     this.galleryMain = document.querySelector('[data-gallery="main"]');
+class Gallery {
+  constructor() {
+    this.gallery = document.querySelector('[data-gallery="gallery"]');
+    this.galleryList = document.querySelectorAll('[data-gallery="list"]');
+    this.galleryMain = document.querySelector('[data-gallery="main"]');
 
-//     this.changeImage = this.changeImage.bind(this);
-//   }
+    this.changeImage = this.changeImage.bind(this);
+  }
 
-//   changeImage({currentTarget}) {
-//     this.galleryMain.src = currentTarget.src;
-//   }
+  changeImage({currentTarget}) {
+    this.galleryMain.src = currentTarget.src;
+  }
 
-//   addChangeEvent() {
-//     this.galleryList.forEach(img => {
-//       img.addEventListener('click', this.changeImage);
-//       img.addEventListener('mouseover', this.changeImage);
-//     })
-//   }
+  addChangeEvent() {
+    this.galleryList.forEach(img => {
+      img.addEventListener('click', this.changeImage);
+      img.addEventListener('mouseover', this.changeImage);
+    })
+  }
 
-//   init() {
-//     if(this.gallery) {
-//       this.addChangeEvent();
-//     }
-//   }
-// }
+  init() {
+    if(this.gallery) {
+      this.addChangeEvent();
+    }
+  }
+}
 
-// const gallery = new Gallery();
-// gallery.init();
+const gallery = new Gallery();
+gallery.init();
 
 
 
